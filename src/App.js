9ce44d95a14d0f385d6418bb32ff4de92f34d2b6
Kaustubh Garud <kaustubh.garud@hexagon.com>
2023-06-15 23:49:44 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import React, { Component } from 'react'
-import navbar from './components/navbar';
+
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        test
       </div>
 
     )
