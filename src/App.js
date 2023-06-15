d9587148e9c2567b7965b5e47d882759525c5cc6
Kaustubh Garud <kaustubh.garud@hexagon.com>
2023-06-15 23:59:17 +0530

    a2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 
 import './App.css';
 import React, { Component } from 'react'
-
+import bootnav from './Components/bootnav';
 
 export default class App extends Component {
   render() {
     return (
+      <>
       <div>
-        test
+<bootnav/>
       </div>
-
+      </>
     )
   }
 }
