44e68d075b5c465e5f4e709c2d127cff4f20cb79
Kaustubh Garud <kaustubh.garud@hexagon.com>
2023-06-17 18:58:03 +0530

    day2

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
+
+  constructor() {
+    super()
+
+    console.log("Hello I'm a construtor from news component");
+    this.state = {
+      articles: [],
+      loading: false
+    }}
+
+    async componentDidMount(){
+      console.log("CDM");
+      let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=9728c6b176774bffb7180afbb4458712";
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData)
+      this.setState({articles:parsedData.articles})
+
+    }
+
+
   render() {
+    console.log("render")
     return (
-      <div>
-        This is the news componenet
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className="container my-3">
+        <h1>NewsMonket - Top Headlines</h1>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+          return <div className="col-md-4" key={element.url}>
+          <NewsItem title={element.title?element.title:" "} description={element.description?element.description: " "} imageUrl={element.urlToImage} newsUrl={element.url} />
+        </div>
+        })}
+          
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
